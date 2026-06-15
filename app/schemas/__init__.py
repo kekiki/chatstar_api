84f5c4ca815d0b5b4699031757573ee7164aa3bb
kejie <kekiki@126.com>
2026-06-15 16:32:56 +0800

    bug fixed

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,6 +1,7 @@
 # Schemas package
 # This package contains Pydantic models for request/response validation
 
-from .google_request import GoogleLoginRequest, ZohoWorkDrive
+from .google_request import GoogleLoginRequest
+from .zoho_workdrive import ZohoWorkDrive
 
 __all__ = ["GoogleLoginRequest", "ZohoWorkDrive"]
