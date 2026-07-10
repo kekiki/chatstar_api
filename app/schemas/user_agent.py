b77b766b757bde5154e72fa7bb9c20dbdaccb391
kejie <kekiki@126.com>
2026-07-10 20:03:44 +0800

    fix

--- app/schemas/user_agent.py
+++ app/schemas/user_agent.py
@@ -4,7 +4,7 @@ class UserAgent(object):
     """
     def __init__(self, user_agent: str):
         infos = user_agent.split(',')
-        # 补齐六段，不足填空字符串
-        while len(infos) < 6:
+        # 补齐七段，不足填空字符串
+        while len(infos) < 7:
             infos.append("")
-        self.app_name, self.app_version, self.build_number, self.deviceModel, self.osVersion, self.brand, self.manufacturer = infos[:6]
+        self.app_name, self.app_version, self.build_number, self.deviceModel, self.osVersion, self.brand, self.manufacturer = infos[:7]
