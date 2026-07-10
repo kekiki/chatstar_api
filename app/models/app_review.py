5b8a22decc425effa0f6df2ee9e70b2fa216e3ce
kejie <kekiki@126.com>
2026-07-10 18:10:27 +0800

    bug fixed

--- app/models/app_review.py
+++ app/models/app_review.py
@@ -4,5 +4,5 @@ from app.database import Base
 class AppReview(Base):
     __tablename__ = "app_review"
     id = Column(Integer, primary_key=True)
-    package_name = Column(String(100))
-    app_version = Column(String(16))
+    package_name = Column(String(100), index=True)
+    app_version = Column(String(16), index=True)
